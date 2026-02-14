6d30f2f46c08d90db1d971f61d76cf600c6a4b86
Carlos Galindo <cgalvez2010@gmail.com>
2026-02-13 21:51:09 -0600

    .

--- lib/helpers/imageExtrac.dart
+++ lib/helpers/imageExtrac.dart
@@ -1,15 +1,44 @@
+import 'dart:io'; //librearia para herramientas internas y externas
 import 'package:flutter/material.dart';
 import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
 import 'package:image_picker/image_picker.dart';
 
-Future<void> pickImageFromGallery(
-  ImageSource source,
-) async {
-  final pickedFile = await pickImageFromGallery(source);
-  if (pickedFile != null) {
-    setState(() {
-      imagenArchivo = File(pickedFile.path);
-    });
-    processImage();
+File? imagenArchivo;
+final ImagePicker picker = ImagePicker();
+String textoExtraido = "";
+
+  Future<void> pickImageFromGallery() async {
+    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+    if (pickedFile != null) {
+      setSState(() {
+        imagenArchivo = File(pickedFile.path);
+      });
+      processImage();
+    }
   }
-}
+
+  Future<void> pickImageFromCamera() async {
+    final pickedFile = await picker.pickImage(source: ImageSource.camera);
+    if (pickedFile != null) {
+      setState(() {
+        imagenArchivo = File(pickedFile.path);
+      });
+      processImage();
+    }
+  } //funcion para la camara
+
+    Future<void> processImage() async {
+    if (imagenArchivo == null) return;
+
+    final inputImage = InputImage.fromFilePath(imagenArchivo!.path);
+    final textRecognizer = TextRecognizer();
+    final RecognizedText recognizedText = await textRecognizer.processImage(
+      inputImage,
+    );
+
+    setState(() {
+      textoExtraido = recognizedText.text;
+    });
+
+    textRecognizer.close();//liberar recursos opcional
+  } // Procesar imagen con OCR
